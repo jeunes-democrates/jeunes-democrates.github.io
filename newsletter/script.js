fcf96034601d62cc426e3d53495d514247634b77
Brachamul <barnaby.brachamul@gmail.com>
2017-01-30 20:47:36 +0100

    componentizing

--- newsletter/script.js
+++ newsletter/script.js
@@ -4,7 +4,8 @@ function vueSetup() {
 		state: {
 			'newsletter': {
 				'meta' : false,
-				'edito' : false
+				'edito' : false,
+				'articles' : []
 			}
 		},
 		mutations: {
@@ -30,8 +31,149 @@ function vueSetup() {
 				store.commit('updateNewsletter', {'edito': edito})
 			})
 		}
+
 	})
 	
+	Vue.http.get(_airTable.ListEndpoint('Actus')).then((response) => {
+		var actus = _airTable.Clean(response.body.records).slice(0,3) // get first 3 actus
+		store.commit('updateNewsletter', {'actus': actus})
+	})
+
+Vue.component('newsletter-header', {
+	template : `
+		<table cellpadding="0" cellspacing="0" border="0" align="center" width="582" style="
+			font-family: 'Trebuchet MS', Helvetica, sans-serif;
+			line-height: 24px;
+			color: #333;
+		">
+
+			<tr>
+				<td valign="top" width="263px" style="color: white;">
+					Bonne année 2017 !
+				</td>
+				<td valign="top" width="263px" style="text-align: right; color: #999;">
+					<!-- empty -->
+				</td>
+			</tr>
+
+		</table>
+		`
+})
+
+Vue.component('newsletter-illustration', {
+	props: ['illustration'],
+	template: `
+		<table v-if="illustration" cellpadding="0" cellspacing="0" border="0" align="center" width="610">
+			<tr>
+				<td>
+					<img :src="illustration[0].url" width="610" />
+				</td>
+			</tr>
+		</table>
+		`
+})
+
+Vue.component('newsletter-edito', {
+	props: ['edito'],
+	template: `
+		<table cellpadding="14px" cellspacing="0" border="0" align="center" width="582" style="
+			font-family: 'Trebuchet MS', Helvetica, sans-serif;
+			line-height: 24px;
+			color: #333;
+		">
+			<tr class="edito" v-if="edito">
+				<td valign="top">
+					<div v-html="edito.Texte"></div>
+				</td>
+			</tr>
+
+		</table>
+	`
+})
+
+Vue.component('newsletter-meta', {
+	props: ['date'],
+	template: `
+		<table cellpadding="14px" cellspacing="0" border="0" align="center" width="582" style="
+			font-family: 'Trebuchet MS', Helvetica, sans-serif;
+			line-height: 24px;
+			color: #333;
+		">
+
+			<tr class="newsletter__logo">
+				<td valign="top" width="287px" style="text-align: right; color: #999;">
+					<p>{{ date }}</p>
+				</td>
+				<td valign="top" width="287px">
+					<table style="
+						text-align: center;
+						font-family: 'Century Gothic', Futura, Verdana, sans-serif;
+						line-height: 1;
+						font-size: 42px;
+						display: inline-block;
+						margin-top: 14px;
+						">
+						<tr>
+							<td style="
+							background-color:#ff6000;
+							color:white;
+							text-align:right;
+							vertical-align:bottom;
+							padding-top:8px;
+							padding-right:2px;
+							padding-left:1px;
+							">
+								J<br>DEM
+							</td>
+							<td style="
+								color:#ff6000;
+								text-align:left;
+								vertical-align:bottom;
+								padding-top:8px;
+								padding-left:1px;
+								width:77px;
+								">
+								EUNES<br>OCRATES
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+
+	`
+})
+
+Vue.component('newsletter-actu', {
+	props: ['actu'],
+	template: `
+		<td class="actu" valign="top">
+			<div v-html="actu.Titre"></div>
+			<img :src="actu.Illustration[0].thumbnails.large.url" width=156 />
+		</td>
+	`
+})
+
+Vue.component('newsletter-actus', {
+	props: ['actus'],
+	template: `
+		<table cellpadding="14px" cellspacing="0" border="0" align="center" width="582" style="
+			font-family: 'Trebuchet MS', Helvetica, sans-serif;
+			line-height: 18px;
+			font-size: 14px;
+			color: #333;
+		">
+			<tr><td colspan="3"><h2>Publications récentes</h2></td><tr>
+			<tr class="actus" v-if="actus">
+				<newsletter-actu :actu="actus[0]"></newsletter-actu>
+				<newsletter-actu :actu="actus[1]"></newsletter-actu>
+				<newsletter-actu :actu="actus[2]"></newsletter-actu>
+			</tr>
+
+		</table>
+	`
+})
+
 	// App
 	var app = new Vue({
 		el: '.newsletter',
@@ -45,112 +187,28 @@ function vueSetup() {
 			<tr>
 				<td>
 
+				<newsletter-header></newsletter-header>
 
-				<!-- Header -->
-				<table cellpadding="0" cellspacing="0" border="0" align="center" width="582" style="
-					font-family: 'Trebuchet MS', Helvetica, sans-serif;
-					line-height: 24px;
-					color: #333;
-				">
+				<newsletter-illustration
+					:illustration="newsletter.meta.Illustration"
+					></newsletter-illustration>
 
-					<tr>
-						<td valign="top" width="263px" style="color: white;">
-							Bonne année 2017 !
-						</td>
-						<td valign="top" width="263px" style="text-align: right; color: #999;">
-							<!-- empty -->
-						</td>
-					</tr>
-				</table>
+				<newsletter-edito
+					:edito="newsletter.edito"
+					></newsletter-edito>
 
+				<hr/>
 
-
-				<!-- Illustration -->
-				<table v-if="newsletter.meta.Illustration" cellpadding="0" cellspacing="0" border="0" align="center" width="610">
-
-					<tr>
-						<td>
-							<img :src="newsletter.meta.Illustration[0].url" width="610" />
-						</td>
-					</tr>
-
-				</table>
-
-
-
-				<!-- Edito -->
-				<table cellpadding="14px" cellspacing="0" border="0" align="center" width="582" style="
-					font-family: 'Trebuchet MS', Helvetica, sans-serif;
-					line-height: 24px;
-					color: #333;
-				">
-					<tr class="edito" v-if="newsletter.edito">
-						<td valign="top">
-							<div v-html="newsletter.edito.Texte"></div>
-						</td>
-					</tr>
-
-				</table>
-
-
-				<!-- Logo et date-->
-				<table cellpadding="14px" cellspacing="0" border="0" align="center" width="582" style="
-					font-family: 'Trebuchet MS', Helvetica, sans-serif;
-					line-height: 24px;
-					color: #333;
-				">
-
-					<tr class="newsletter__logo">
-						<td valign="top" width="287px" style="text-align: right; color: #999;">
-							<p>{{ newsletter.meta.Date }}</p>
-						</td>
-						<td valign="top" width="287px">
-							<table style="
-								text-align: center;
-								font-family: 'Century Gothic', Futura, Verdana, sans-serif;
-								line-height: 1;
-								font-size: 42px;
-								display: inline-block;
-								margin-top: 14px;
-								">
-								<tr>
-									<td style="
-									background-color:#ff6000;
-									color:white;
-									text-align:right;
-									vertical-align:bottom;
-									padding-top:8px;
-									padding-right:2px;
-									padding-left:1px;
-									">
-										J<br>DEM
-									</td>
-									<td style="
-										color:#ff6000;
-										text-align:left;
-										vertical-align:bottom;
-										padding-top:8px;
-										padding-left:1px;
-										width:77px;
-										">
-										EUNES<br>OCRATES
-									</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				</table>
-
-
+				<newsletter-actus
+					:actus="newsletter.actus"
+					></newsletter-actus>
 
 				</td>
 			</tr>
 			</table>
 
 			</div>
-
-
-			
+	
 		`,
 		computed: {
 			newsletter () {
@@ -160,3 +218,8 @@ function vueSetup() {
 	})
 
 }
+
+
+//				<newsletter-meta
+//					:date="newsletter.meta.Date"
+//					></newsletter-meta>
