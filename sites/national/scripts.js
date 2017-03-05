aef4e60e0c960046de6c13d3e95e7e0fb17cc56f
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2017-03-05 23:45:11 +0100

    logo

--- sites/national/scripts.js
+++ sites/national/scripts.js
@@ -39,48 +39,46 @@ function vueSetup() {
 	Vue.component('main-nav', {
 		props: ['brand', 'menu'],
 		template: `
-			<nav class="navbar navbar-light">
-				<div class="container">
-					<a class="navbar-brand" href="#">
+			<div class="container">
+				<nav class="navbar navbar-light navbar-toggleable-md">
+<!--				<a class="navbar-brand" href="#">
 						<img src="logo.png" class="navbar__logo">
-						<span v-html="brand"></span>
 					</a>
-					<ul class="nav">
-						<span v-for="menuItem in menu">
-							<li v-if="menuItem.hasOwnProperty('url')" class="nav-item">
-								<a
-									v-html="menuItem.name"
-									:href="menuItem.url"
-									class="nav-link"
-									>
-								</a>
-							</li>
-							<li v-else-if="menuItem.hasOwnProperty('children')" class="nav-item dropdown" >
-								<a
-									v-html="menuItem.name"
-									href="#"
-									class="nav-link dropdown-toggle"
-									data-toggle="dropdown"
-									role="button" aria-haspopup="true" aria-expanded="false"
-									>
-								</a>
-								<div class="dropdown-menu">
-									<a class="dropdown-item" href="#">Action</a>
-									<a class="dropdown-item" href="#">Another action</a>
-									<a class="dropdown-item" href="#">Something else here</a>
-									<div class="dropdown-divider"></div>
-									<a class="dropdown-item" href="#">Separated link</a>
-								</div>
-							</li>
-						</span>
-					</ul>
-				</div>
-			</nav>
+-->					<div class="collapse navbar-collapse">
+						<ul class="navbar-nav">
+							<span v-for="menuItem in menu">
+								<li v-if="menuItem.hasOwnProperty('url')" class="nav-item">
+									<a
+										v-html="menuItem.name"
+										:href="menuItem.url"
+										class="nav-link"
+										>
+									</a>
+								</li>
+								<li v-else-if="menuItem.hasOwnProperty('children')" class="nav-item dropdown" >
+									<a
+										v-html="menuItem.name"
+										href="#"
+										class="nav-link dropdown-toggle"
+										data-toggle="dropdown"
+										role="button" aria-haspopup="true" aria-expanded="false"
+										>
+									</a>
+									<div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Action</a>
+										<a class="dropdown-item" href="#">Another action</a>
+										<a class="dropdown-item" href="#">Something else here</a>
+										<div class="dropdown-divider"></div>
+										<a class="dropdown-item" href="#">Separated link</a>
+									</div>
+								</li>
+							</span>
+						</ul>
+					</div>
+				</nav>
+			</div>
 		`
 	})
-
-
-
 
 
 
@@ -92,11 +90,56 @@ function vueSetup() {
 
 			<div id="app" lang="fr">
 
-				<div class="top-background"></div>
+				<div class="top-background">
+
+					<div class="container">
+
+						<h1>
+							<img class="title__image" src="long-logo-white.png" :alt="state.meta.name" />
+						</h1>
+
+					</div>
+
+				</div>
 
 				<main-nav :brand="state.meta.name" :menu="state.menu"></main-nav>
 
-				<h1>{{ state.meta.name }}</h1>
+				
+
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
+				<p>Lorem Ipsum</p>
 
 			</div>
 	
