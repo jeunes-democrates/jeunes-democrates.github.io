1e15f1f73c04bc10dc46ab8c8508b421205c27a5
Brachamul <barnaby.brachamul@gmail.com>
2017-07-11 10:05:05 +0200

    more stuff

--- sites/locaux/script.js
+++ sites/locaux/script.js
@@ -4,7 +4,21 @@ function vueSetup() {
 		state: {
 			'site': {
 				'name': "Jeunes Démocrates d'Alsace",
-				'description' : "Groupe local représentant les jeunes adhérents du Mouvement Démocrate",
+				'description': "Groupe local représentant les jeunes adhérents du Mouvement Démocrate",
+				'nav': true,
+				'modules': {
+					'intro': {
+						'title': 'Présentation',
+						'text': `
+**Lorem ipsum dolor sit amet**, consectetur adipiscing elit. Morbi nec vehicula risus. Nam lacus neque, tristique in dignissim et, tristique vitae nibh. Phasellus hendrerit rhoncus eros, quis aliquam ex maximus sit amet. Vestibulum sapien felis, consequat sit amet efficitur quis, congue et magna. Mauris tempor vehicula turpis, eu viverra quam egestas quis. In eu nulla eu sapien molestie feugiat ut sit amet metus. Fusce bibendum nunc risus, a ornare enim posuere in. Nunc hendrerit pharetra justo vitae gravida. Donec interdum vehicula auctor. Vestibulum consequat ex vulputate, porta felis et, tincidunt ex. Nullam ac condimentum elit. Vestibulum at porttitor orci. Duis molestie blandit ipsum et molestie.
+
+Vivamus maximus faucibus metus, in semper nunc [faucibus](http://twitter.com) id. Praesent et velit molestie ante venenatis condimentum vel quis orci. Ut est tortor, interdum id sapien at, convallis dignissim enim. Pellentesque fringilla sollicitudin mi, sagittis finibus tellus fermentum vitae. Donec ut elit sem. Phasellus laoreet dolor sit amet augue eleifend ullamcorper. Sed aliquet bibendum accumsan. Ut vehicula nunc id interdum pretium. Praesent in quam pellentesque magna iaculis aliquet. Donec iaculis tincidunt dolor sit amet semper. Donec vulputate velit eget dapibus fringilla. Proin vitae efficitur urna. Vestibulum eget euismod purus, a pellentesque nibh. Vestibulum pretium fringilla ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quis pulvinar nulla. 
+						`
+					},
+					'team': false,
+					'contact': false,
+//					'news': false, // not yet ready
+				},
 				'facebookFeed': 'jeunes-democrates',
 				'twitterFeed': 'J_Democrates'
 			}
@@ -18,20 +32,44 @@ function vueSetup() {
 		}
 	})
 
-	Vue.component('main-nav', {
-		props: ['brand'],
+	Vue.component('site-intro', {
+		props: ['intro'],
 		template: `
-			<nav class="navbar navbar-light">
-				<div class="container">
-					<a class="navbar-brand" href="#">
-						<img src="logo.png" class="navbar__logo">
-						<span v-html="brand"></span>
-					</a>
-				</div>
-			</nav>
+			<section class="site-intro">
+
+				<h2 v-if="intro.title" class="section-heading intro__title">
+					{{ intro.title }}
+				</h2>
+
+				<div class="intro__text" v-html="marked(intro.text)"></div>
+
+			</section>
 		`
 	})
 
+	Vue.component('site-news', {
+		// This module is not ready for use yet,
+		// as it needs to load the actual articles,
+		// which we can't handle yet
+		props: ['news'],
+		template: `
+			<section class="site-news">
+
+				<h2 class="section-heading">
+					Actualités
+				</h2>
+
+				<div class="news__list">
+					<div v-for"newsItem in news.items" class="news__item">
+						<div class="news__item__illustration"></div>
+						<div class="news__item__title"></div>
+						<div class="news__item__description"></div>
+					</div>
+				</div>
+
+			</section>
+		`
+	})
 
 
 
@@ -47,10 +85,14 @@ function vueSetup() {
 
 				<section v-if="site.name || site.description" class="header">
 
-					<h1 class="site-header">
+					<h1 v-if="site.name || site.description" class="site-header">
 						<span v-if="site.name" class="site-name">{{ site.name }}</span>
 						<span v-if="site.description" class="site-description">{{ site.description }}</span>
 					</h1>
+
+				</section>
+
+				<section v-if="site.nav" class="nav-section" >
 
 					<nav class="nav">
 						<span class="nav__item">
@@ -58,28 +100,26 @@ function vueSetup() {
 								Actualités
 							</a>
 						</span>
+						<span class="nav__spacer">·</span>
 						<span class="nav__item">
 							<a class="nav__link" href="#">
 								L'équipe
 							</a>
 						</span>
+						<span class="nav__spacer">·</span>
 						<span class="nav__item">
 							<a class="nav__link" href="#">
 								Contactez-nous
 							</a>
 						</span>
+						<span class="nav__spacer">·</span>
 					</nav>
 
-
 				</section>
 
-				<section>
+				<site-intro v-if="site.modules.intro" :intro="site.modules.intro"></site-intro>
 
-					<h2 class="section-heading">
-						Actualités
-					</h2>
-
-				</section>
+				<site-news v-if="site.modules.news" :news="site.modules.news"></site-news>
 
 				<section>
 
